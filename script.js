dacbe7c5495122de4d5128b54cb7dad48b84d35b
kimeunbeen <eunbin1235@gmail.com>
2025-02-26 21:56:33 +0900

    url 수정

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const API_KEY = "eb0514986c0a4e94b99595bc04e58b9b";
 let newsList = [];
 
 const getLatesNews = async () => {
-  const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-  );
+  // const url = new URL(
+  //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  // );
 
   // 제출용 url
-  // url = new URL(
-  //   `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
-  // );
+  url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/`);
   const response = await fetch(url);
   const data = await response.json();
 
